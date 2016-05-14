edf43e0edfd59b26fec23c2fae4df343b6ea5445
MouryaKumar Reddy <mourya.rajala@gmail.com>
2016-05-13 19:07:46 -0500

    Adding js-beautify

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 var mongodb = require('mongodb');
 var movies = require('./movies');
+var beautify = require('js-beautify').js_beautify,
+fs = require('fs');
+ 
+fs.readFile('interface.js', 'utf8', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    fs.writeFile('interface.js',beautify(data, { indent_size: 2 }),'utf8')
+    //console.log(beautify(data, { indent_size: 2 }));
+});
